46b38aa99adb8c31b1c17847857de2d945d16a2a
Tom S. <thoschulte@googlemail.com>
2024-03-03 21:54:10 +0100

    refactor: adjust logic for better performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import {ChangeDetectionStrategy, Component, signal, WritableSignal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooComponent} from "./foo/foo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent {
   public title: string = 'angular-v17-signals';
   protected counter: WritableSignal<number> = signal<number>(0);
+  protected message: string[] = [];
 
   protected increment(): void {
     // this.counter++;
-    this.counter.set(this.counter() + 1);
+    const val: number = this.counter() + 1;
+    this.message.push(val.toString());
+    this.counter.set(val);
   }
 }
