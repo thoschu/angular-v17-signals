fba840ec51e348f3aece9e5227a568761af639ac
Tom S. <thoschulte@googlemail.com>
2024-03-06 10:20:37 +0100

    feat: add input signal and so on

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, Input, InputSignal, Signal } from '@angular/core';
 
 import { AppService } from '../app.service';
 
@@ -11,10 +11,22 @@ import { AppService } from '../app.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooComponent {
-  @Input({required: true})
-  public message: string[] = [];
+  // @Input({required: true})
+  // public message: string[] = [];
+  public readonly message: InputSignal<string[] | undefined> = input<string[]>();
+  protected readonly derivedMessage: Signal<number>;
 
-  constructor(protected readonly appService: AppService) {}
+  constructor(protected readonly appService: AppService) {
+    this.derivedMessage = computed((): number => {
+      const messageList: string[] | undefined = this.message();
+
+      if (messageList) {
+        return messageList.length * appService.counter();
+      }
+
+      return 0;
+    });
+  }
 
   protected reset(): void {
     this.appService.resetCounter();
