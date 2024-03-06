918aad2a4b6e3ac8b67ada4ae2944ad535924c0c
Tom S. <thoschulte@googlemail.com>
2024-03-06 14:22:44 +0100

    feat: add transform to input signal

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, computed, input, Input, InputSignal, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  Input,
+  InputSignal,
+  InputSignalWithTransform,
+  Signal
+} from '@angular/core';
 
 import { AppService } from '../app.service';
 
@@ -14,9 +23,15 @@ export class FooComponent {
   // @Input({required: true})
   // public message: string[] = [];
   // public message: InputSignal<string[]> = input<string[]>([]);
-  // public message: InputSignal<string[]> = input<string[]>([], {alias: 'messageAlias',});
-  public message: InputSignal<string[]> = input.required<string[]>({ alias: 'messageAlias' });
+  // public message: InputSignal<string[]> = input<string[]>([], {alias: 'messageAlias'});
+  public message: InputSignal<string[]> = input.required<string[]>({
+    alias: 'messageAlias'
+  });
   protected readonly derivedMessage: Signal<number>;
+  public readonly disabled: InputSignalWithTransform<boolean, string | boolean> = input<boolean, string | boolean>(true, {
+    alias: 'resetDisabled',
+    transform: (value: boolean | string): boolean => (typeof value === 'string') ? (value === '') : value
+  });
 
   constructor(protected readonly appService: AppService) {
     this.derivedMessage = computed((): number => {
