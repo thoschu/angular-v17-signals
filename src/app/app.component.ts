2801ed9fd8dc3585f48310f032b64a97b539a9e8
Tom S. <thoschulte@googlemail.com>
2024-03-08 15:17:34 +0100

    feat: add default @defer (when the browser is idle)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PdfComponent } from './pdf/pdf.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PdfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   public title: string = 'angular-v17-signals';
-
 }
--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-pdf',
   standalone: true,
-  imports: [],
+  imports: [
+    MatExpansionModule
+  ],
   templateUrl: './pdf.component.html',
   styleUrl: './pdf.component.scss'
 })
 export class PdfComponent {
+  constructor() {
+    const controller: AbortController = new AbortController();
+    const signal: AbortSignal = controller.signal;
 
+    console.log(signal);
+  }
 }
