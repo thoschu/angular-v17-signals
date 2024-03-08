8b332d9fddebdd3cded364302f051f8fbc00fc39
Tom S. <thoschulte@googlemail.com>
2024-03-08 16:15:13 +0100

    feat: add @placeholder with delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 import { PdfComponent } from './pdf/pdf.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PdfComponent],
+  imports: [RouterOutlet, PdfComponent, MatProgressSpinner],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
