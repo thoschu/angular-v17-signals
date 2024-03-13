c8eed4f2bf61c532e306361eeb2ab9ce35c9b5f1
Tom S. <thoschulte@googlemail.com>
2024-03-13 14:30:06 +0100

    refactor: update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,43 @@
+import {AsyncPipe, JsonPipe, NgForOf, NgIf} from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { fromEvent, interval, noop, Observable, Subscription, timer } from 'rxjs';
+import { fromEvent, interval, map, noop, Observable, Subscription, tap, timer } from 'rxjs';
 
-import { AppService, Posts } from './app.service';
+import {AppService, Post, /*Comments, Posts, Profile*/} from './app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, NgIf, NgForOf, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  public title: string = 'angular-v17-signals';
+  public readonly title: string = 'angular-v17-signals';
+  private readonly click$: Observable<Event> = fromEvent<Event>(document, 'click');
+  protected readonly posts$:  Observable<any> = this.appService.getPosts();
+  protected readonly comments$: Observable<any> = this.appService.getComments();
 
   constructor(private readonly appService: AppService) {
-    appService.getPosts().subscribe(
+    appService.getPosts().pipe(
+        tap(console.dir)
+    ).subscribe(
         console.dir,
         noop,
         (): void => console.info('complete')
     );
+
+    // this.appService.getProfile()
+    //   .pipe(
+    //     tap<Profile>(console.log),
+    //   )
+    //   .subscribe(noop);
+
+    // this.appService.getComments().pipe().subscribe(
+    //     console.dir,
+    //     noop,
+    //     (): void => console.info('complete')
+    // );
   }
 
   public ngOnInit(): void {
@@ -36,16 +54,28 @@ export class AppComponent implements OnInit {
     //   }, 1000);
     // });
 
-    const click$: Observable<Event> = fromEvent<Event>(document, 'click');
-    const interval$: Observable<number> = interval(1000);
-    const timer$: Observable<number> = timer(3000, 1000);
+    // {
+    //   const timer$: Observable<number> = timer(3000, 1000);
+    //
+    //   setTimeout((sub: Subscription): void => sub.unsubscribe(),7000, timer$.subscribe(
+    //     (value: number): void => console.log(value),
+    //     (err: Error): void => console.error(err),
+    //     (): void => console.info('timer$ complete')
+    //   ));
+    // }
+    // // ---------------------------------------------------------------------------------------
+    // {
+    //   const interval$: Observable<number> = interval(1000);
+    //
+    //   setTimeout((sub: Subscription): void => sub.unsubscribe(), 9000, interval$.pipe(
+    //       tap(console.log),
+    //       map((val: number ): number => val * 10),
+    //       tap(console.log)
+    //   ).subscribe(noop));
+    // }
+  }
 
-    const sub: Subscription = timer$.subscribe(
-        (value: number): void => console.log(value),
-        (err: Error): void => console.error(err),
-        (): void => console.info('complete')
-    );
-
-    setTimeout((): void => sub.unsubscribe(), 7000);
+  protected trackById(index: number, post: Post) { // 👈
+    return post.id;
   }
 }
