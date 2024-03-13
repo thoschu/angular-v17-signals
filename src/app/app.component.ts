57513ba333e16c1b7a00967b3a4170dd23f94efb
Tom S. <thoschulte@googlemail.com>
2024-03-13 19:16:43 +0100

    feat: add sharereplay operator
    ❗ https://www.learnrxjs.io/learn-rxjs/operators/multicasting/sharereplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { fromEvent, interval, map, noop, Observable, Subscription, tap, timer } from 'rxjs';
+import {fromEvent, interval, map, noop, Observable, shareReplay, Subscription, tap, timer} from 'rxjs';
 import {} from 'ramda';
 
 import { AppService, Comment, Comments, Post, Posts, /*Comments, Posts, Profile*/ } from './app.service';
@@ -16,27 +16,44 @@ import { AppService, Comment, Comments, Post, Posts, /*Comments, Posts, Profile*
 export class AppComponent implements OnInit {
   public readonly title: string = 'angular-v17-signals';
   private readonly click$: Observable<Event> = fromEvent<Event>(document, 'click');
-  protected readonly posts$:  Observable<Posts> = this.appService.getPosts();
-  protected readonly postsFilteredGreaterThan$: Observable<Posts> = this.appService.getPosts().pipe(
+  private readonly _posts$: Observable<Posts> = this.appService.getPosts().pipe(shareReplay());
+  protected readonly posts$: Observable<Posts> = this.appService.getPosts();
+  protected readonly postsFilteredGreaterThan$: Observable<Posts> = this._posts$.pipe(
       map(
           (posts: Posts) => posts.filter((post: Post): boolean => post.views > 200)
       )
   );
-  protected readonly postsFilteredLessThan$: Observable<Posts> = this.appService.getPosts().pipe(
+  protected readonly postsFilteredLessThan$: Observable<Posts> = this._posts$.pipe(
       map(
           (posts: Posts) => posts.filter((post: Post): boolean => post.views < 300)
       )
   );
-  protected readonly comments$: Observable<Comments> = this.appService.getComments();
+  protected readonly comments$: Observable<Comments> = this.appService.getComments()
+      .pipe(
+          shareReplay(),
+          tap(console.log) // ❗❗❗
+      );
+  protected readonly commentsFilteredLess$: Observable<Comments> =
+      this.comments$.pipe(map((comments: Comments) => comments.filter((comment: Comment): boolean => {
+        return comment.id < 3;
+      })));
+
+  protected readonly commentsFilteredGreater$: Observable<Comments> =
+      this.comments$.pipe(map((comments: Comments) => comments.filter((comment: Comment): boolean => {
+        return comment.id > 2;
+      })));
 
   constructor(private readonly appService: AppService) {
-    appService.getPosts().pipe(
-        tap(console.dir)
-    ).subscribe(
-        console.dir,
-        noop,
-        (): void => console.info('complete')
-    );
+    this.commentsFilteredLess$.subscribe(noop);
+    this.commentsFilteredGreater$.subscribe(noop);
+
+    // appService.getPosts().pipe(
+    //     tap(console.dir)
+    // ).subscribe(
+    //     console.dir,
+    //     noop,
+    //     (): void => console.info('complete')
+    // );
 
     // this.appService.getProfile()
     //   .pipe(
